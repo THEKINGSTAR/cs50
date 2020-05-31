fe9f95bc714a632cba8e7262373a98d5214604b5
KHALED MOHAMED <THEKINGSTAR5002@GMAIL.COM>
2020-05-31 16:56:21 +0200

    start the c tets on windows

---
+++ ctest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.c>
+
+int main(void)
+{
+    printf("hello world ");
+   /* int number = get_int("please give me number");
+    printf("the number is %i ",number);
+    return 0;
+    */
+}
