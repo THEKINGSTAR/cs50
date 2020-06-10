bea777b1bfb604a9f9df6df03a023d83fc994148
THEKINGSTAR <THEKINGSTAR5002@GMAIL.COM>
2020-06-11 00:56:21 +0200

    finisting the credi card in test.c

--- test.c
+++ test.c
@@ -19,17 +19,17 @@ int main(void)
     // long long number = 378282246310005; //tow valid card  4003600000000014  || 378282246310005
     // second_to_last(number);
     /*--------------------------------------------------*/
-/*--------------------------------------------------*/
+    /*--------------------------------------------------*/
     /*MAIN APP RUNT */
 
-    // validty_check();
-/*--------------------------------------------------*/
-    testing_number();
+    validty_check();
+    /*--------------------------------------------------*/
+    //testing_number();
 }
 
 long long validty_check(void)
 {
-    long long input_number = get_long_long("please enter valid card number : \n ");
+    long long input_number = get_long_long("please enter valid card number : \n");
 
     return lenght_clac(input_number);
 }
@@ -41,10 +41,10 @@ long long lenght_clac(long long number)
     int lenght = 0;
     while ((number / 10) != '\0')
     {
-        lenght ++;
+        lenght = lenght + 1;
         number = number / 10;
     }
-    lenght = lenght +1;
+    lenght = lenght + 1;
     return company_check(inputed_number, lenght);
 }
 /*-------------------------------company check-----------------------------------*/
@@ -53,30 +53,18 @@ long long lenght_clac(long long number)
 long long company_check(long long inputed_number, int lenght)
 {
     int checksum = second_to_last(inputed_number);
-    printf("%i \n", checksum);
+    //printf("recived checksum is : %i \n", checksum);
 
     checksum = (checksum - ((checksum / 10) * 10));
-    printf("lenght is : %i \n ",lenght);
-    int frist_tow_digit = inputed_number / pow(10, lenght -2);
-    printf("the frist tow digit are : %i \n",frist_tow_digit);
-    printf("%i \n",checksum);
-    if (checksum == 0 &  lenght >= 13 & lenght < 17)
+    //printf("lenght is : %i \n",lenght);
+    int frist_tow_digit = inputed_number / pow(10, lenght - 2);
+    //printf("the frist tow digit are : %i \n",frist_tow_digit);
+    //printf("%i \n",checksum);
+if (checksum == 0 &  lenght >= 13 & lenght < 17)
+{
+    if (lenght == 13)
     {
-        if (lenght == 15)
-        {
-            int american = inputed_number / pow(10, lenght - 2);
-            if (american == 34 || american == 37)
-            {
-                printf ("AMEX\n");
-            }
-            else
-            {
-                printf("INVALID\n");
-            }
-        }
-        if (lenght == 13 || lenght == 16)
-        {
-            int Visa = inputed_number / pow(10, lenght - 1);
+        int Visa = inputed_number / pow(10, lenght - 1);
             if (Visa == 4)
             {
                 printf("VISA\n");
@@ -85,26 +73,47 @@ long long company_check(long long inputed_number, int lenght)
             {
                 printf("INVALID\n");
             }
-        }
-        if (lenght == 16)
-        {
-            int master_Card = inputed_number / pow(10, lenght - 2);
-            if (master_Card == 51 || master_Card == 52 || master_Card == 53 || master_Card == 54 || master_Card == 55)
-            {
-                printf("MASTERCARD\n ");
-            }
-            else
-            {
-                printf("INVALID\n");
-            }
-        }
     }
+    else if (lenght == 15)
+        {
+            int american = inputed_number / pow(10, lenght - 2);
+                if (american == 34 || american == 37)
+            {
+                    printf("AMEX\n");
+            }
+                else
+            {
+                    printf("INVALID\n");
+            }
+        }
+    else if (lenght == 16)
+            {
+                int master_Card = inputed_number / pow(10, lenght - 2);
+                int Visa = inputed_number / pow(10, lenght - 1);
+
+                if (master_Card == 51 || master_Card == 52 || master_Card == 53 || master_Card == 54 || master_Card == 55)
+                {
+                    printf("MASTERCARD\n ");
+                }
+                else if (Visa == 4)
+                {
+                    printf("VISA\n");
+                }
+                else
+                {
+                    printf("INVALID\n");
+                }
+            }
+    else
+        {
+            printf("INVALID\n");
+        }
+}
     else
     {
         printf("INVALID\n");
     }
-
-   
+    return 0;
 }
 
 /*------------------calculate the  second to last digit sum----------------------*/
@@ -143,18 +152,14 @@ long long first_digit(long long number, int last_tow_sec_sum)
     }
     sum_first_digit = sum_first_digit + number;
     int total_sum = sum_first_digit + last_tow_sec_sum;
-    printf("%i\n",total_sum);
+    //printf("%i\n",total_sum);
     return total_sum;
 }
 
 
 
 
-
-long long testing_number(void)
-{   
 //------------------------------------------TEST CASES ------------------------------------//
-
 /*
 1-    :) identifies 378282246310005 as AMEX
 2-    :) identifies 371449635398431 as AMEX
@@ -168,6 +173,9 @@ long long testing_number(void)
 10-   :( identifies 5673598276138003 as INVALID
 11-   :( identifies 4111111111111113 as INVALID
 */
+
+/*  long long testing_number(void)
+{
     long long test_number[11] = {378282246310005,371449635398431,5555555555554444,5105105105105100,4111111111111111,4012888888881881
                                 ,1234567890,369421438430814,4062901840,5673598276138003,4111111111111113};
     for (int i = 0; i < 11; i++)
@@ -176,4 +184,4 @@ long long testing_number(void)
         lenght_clac(test_number[i]);
         printf("**************************************** \n");
     }
-}
+}  */
