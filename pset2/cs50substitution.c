f994563ddacd8a2da6161017ebc2a8a8578185c7
THEKINGSTAR <THEKINGSTAR5002@GMAIL.COM>
2020-06-14 04:41:47 +0200

    SUBMITED substitution TO CS50

--- pset2/cs50substitution.c
+++ pset2/cs50substitution.c
@@ -1,6 +1,6 @@
 //substitution
 /*
-A key, for example, might be the string NQXPOMAFTRHLZGECYJIUWSKDVB. 
+A key, for example, might be the string NQXPOMAFTRHLZGECYJIUWSKDVB.
 This 26-character key means that A (the first letter of the alphabet)
   should be converted into N (the first character of the key), B (the second letter of the alphabet)
   should be converted into Q (the second character of the key), and so forth.
@@ -13,16 +13,16 @@ This 26-character key means that A (the first letter of the alphabet)
 #include <stdlib.h>
 
 int key_validate(string input_key);
-int text_to_cypher(int key);
-int assciconvert(input_text, key);
-string cipher(string text, int key);
+int text_to_cypher(string key);
+int assciconvert(string input_text, string key);
+//string cipher(string text, int key);
 
 /*-----------------------Take key    from user--------------------------------------------------*/
 int main(int argc, string argv[])
 {
     if (argc == 2)
     {
-        key_validate(argv[1]);
+       return key_validate(argv[1]);
     }
     else
     {
@@ -34,33 +34,33 @@ int main(int argc, string argv[])
 /*-----------------------check if the lenght of the user input is 26 digit ---------------------*/
 //non repeated alpha
 //no numbers in the key
-int key_validate (string input_key)
+int key_validate(string input_key)
 {
     int count_key_letters = 0;
-    int check_repated_letter =0;
-    int check_digit_in_key =0;
+    int check_repated_letter = 0;
+    int check_digit_in_key = 0;
     int key_kenght = strlen(input_key);
-    for (int i = 0; i < key_kenght ; i++)
+    for (int i = 0; i < key_kenght; i++)
     {
         if (isalpha(input_key[i]))
         {
             for (int j = 0; j < i; j++)
             {
-                if ( input_key[j] == input_key[i] )
+                if (input_key[j] == input_key[i])
                 {
-                    check_repated_letter ++ ;
+                    check_repated_letter++;
                 }
             }
         }
         count_key_letters++;
-//printf("%i\n", count_key_letters);
-        if(isdigit(input_key[i]))
+        //printf("%i\n", count_key_letters);
+        if (isdigit(input_key[i]))
         {
-            check_digit_in_key ++ ;
+            check_digit_in_key++;
         }
     }
-//printf("%i\n",count_key_letters);
-    if (count_key_letters == 26 )
+    //printf("%i\n",count_key_letters);
+    if (count_key_letters == 26)
     {
         if (check_digit_in_key > 0)
         {
@@ -76,6 +76,7 @@ int key_validate (string input_key)
         else
         {
             text_to_cypher(input_key);
+            return 0;
         }
     }
     else
@@ -88,24 +89,26 @@ int key_validate (string input_key)
 
 /*-------------------------------take text from user  to cipher -------------------------------------*/
 
-int text_to_cypher(key)
+int text_to_cypher(string key)
 {
-    string input_text = get_string("", "plaintext:\n");
+    string input_text = get_string("","plaintext: ");
+    printf("ciphertext: ");
     assciconvert(input_text, key);
+    printf("\n");
     return 0;
 }
 
 /*------------------------------assciconvert -------------------------------------*/
 //https://www.asciichart.com/
 
-int assciconvert(string input_text,string key)
+int assciconvert(string input_text, string key)
 {
     /*
     string key = "nQXPOMAFTRHLZGECYJIUWSKDVB";
     string input_text = "Hello";
     */
 
-    string array_of_keys[26];
+    int array_of_keys[26];
     for (int i = 0; i < 26; i++)
     {
         //to_convert[i] = atoi(text[i]);
@@ -113,54 +116,56 @@ int assciconvert(string input_text,string key)
         //converted every givin charc to number
     }
 
-    string asscii_lower_case[26];
+    int asscii_lower_case[26];
     for (int l = 0; l < 26; l++)
     {
         asscii_lower_case[l] = 97 + l;
         //printf("%c,", asscii_lower_case[l]);
     }
-        //printf("\n");
-    string asscii_upper_case[26];
+    //printf("\n");
+    int asscii_upper_case[26];
     for (int u = 0; u < 26; u++)
     {
         asscii_upper_case[u] = 65 + u;
         //printf("%c,", asscii_upper_case[u]);
     }
-        //printf("\n");
+    //printf("\n");
     /*
         array_of_asscii_code(lower or upper);
-        
+
         [#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#]
-        
+
         array_of_keys;
 
         [#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#]
-        
+
         array_of_input;
-        
+
         [#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#,#]
 
     */
 
     //convert every character in the asscii to its key
 
-/*
-string converted_asscii_upper_case[26];
-string converted_asscii_lower_case[26];
-*/
-    string converted_asscii_table[26];
+    /*
+    string converted_asscii_upper_case[26];
+    string converted_asscii_lower_case[26];
+    */
+    int converted_asscii_table[26];
     for (int c = 0; c < 26; c++)
     {
-        /*     if (isupper(array_of_keys[c]))
-    {
-        converted_asscii_upper_case[c] = array_of_keys[c];
-        printf("%i,", converted_asscii_upper_case[c]);
-    }
-    if (islower(array_of_keys[c]))
-    {
-        converted_asscii_lower_case[c] = array_of_keys[c];
-        printf("%i,", converted_asscii_lower_case[c]);
-    } */
+        /*
+        if (isupper(array_of_keys[c]))
+        {
+            converted_asscii_upper_case[c] = array_of_keys[c];
+            printf("%i,", converted_asscii_upper_case[c]);
+        }
+        if (islower(array_of_keys[c]))
+        {
+            converted_asscii_lower_case[c] = array_of_keys[c];
+            printf("%i,", converted_asscii_lower_case[c]);
+        }
+        */
         converted_asscii_table[c] = array_of_keys[c];
     }
     //loop in the lenght of the string
@@ -178,14 +183,14 @@ string converted_asscii_lower_case[26];
                 {
                     if (charcter_in_text == asscii_upper_case[c])
                     {
-                        printf("ciphertext: %c", toupper(converted_asscii_table[c]));
+                        printf("%c", toupper(converted_asscii_table[c]));
                     }
                 }
                 if (islower(charcter_in_text))
                 {
                     if (charcter_in_text == asscii_lower_case[c])
                     {
-                        printf("ciphertext: %c", tolower(converted_asscii_table[c]));
+                        printf("%c", tolower(converted_asscii_table[c]));
                     }
                 }
             }
@@ -201,36 +206,23 @@ string converted_asscii_lower_case[26];
 
     return 0;
 }
-
 /*################ TEST CASES ################################*/
 /*
 :) substitution.c exists
 :) substitution.c compiles
-:( encrypts "A" as "Z" using ZYXWVUTSRQPONMLKJIHGFEDCBA as key
-    expected "ciphertext: Z\...", not "Z"
-:( encrypts "a" as "z" using ZYXWVUTSRQPONMLKJIHGFEDCBA as key
-    expected "ciphertext: z\...", not "z"
-:( encrypts "ABC" as "NJQ" using NJQSUYBRXMOPFTHZVAWCGILKED as key
-    expected "ciphertext: NJ...", not "NJQ"
-:( encrypts "XyZ" as "KeD" using NJQSUYBRXMOPFTHZVAWCGILKED as key
-    expected "ciphertext: Ke...", not "KeD"
-:( encrypts "This is CS50" as "Cbah ah KH50" using YUKFRNLBAVMWZTEOGXHCIPJSQD as key
-    expected "ciphertext: Cb...", not "Cbah ah KH50"
-:( encrypts "This is CS50" as "Cbah ah KH50" using yukfrnlbavmwzteogxhcipjsqd as key
-    expected "ciphertext: Cb...", not "Cbah ah KH50"
-:( encrypts "This is CS50" as "Cbah ah KH50" using YUKFRNLBAVMWZteogxhcipjsqd as key
-    expected "ciphertext: Cb...", not "Cbah ah KH50"
-:( encrypts all alphabetic characters using DWUSXNPQKEGCZFJBTLYROHIAVM as key
-    expected "ciphertext: Rq...", not "Rqx tokug wlji..."
+:) encrypts "A" as "Z" using ZYXWVUTSRQPONMLKJIHGFEDCBA as key
+:) encrypts "a" as "z" using ZYXWVUTSRQPONMLKJIHGFEDCBA as key
+:) encrypts "ABC" as "NJQ" using NJQSUYBRXMOPFTHZVAWCGILKED as key
+:) encrypts "XyZ" as "KeD" using NJQSUYBRXMOPFTHZVAWCGILKED as key
+:) encrypts "This is CS50" as "Cbah ah KH50" using YUKFRNLBAVMWZTEOGXHCIPJSQD as key
+:) encrypts "This is CS50" as "Cbah ah KH50" using yukfrnlbavmwzteogxhcipjsqd as key
+:) encrypts "This is CS50" as "Cbah ah KH50" using YUKFRNLBAVMWZteogxhcipjsqd as key
+:) encrypts all alphabetic characters using DWUSXNPQKEGCZFJBTLYROHIAVM as key
 :) handles lack of key
-:( handles invalid key length
-    expected exit code 1, not 0
-:( handles invalid characters in key
-    expected exit code 1, not 0
-:( handles duplicate characters in key
-    expected exit code 1, not 0
-:( handles multiple duplicate characters in key
-    expected exit code 1, not 0
+:) handles invalid key length
+:) handles invalid characters in key
+:) handles duplicate characters in key
+:) handles multiple duplicate characters in key
 */
 
 /*
@@ -249,8 +241,6 @@ For security, you’ll see asterisks (*) instead of the actual characters in you
 //--------------------------------------   submit50 cs50/problems/2020/x/substitution
 */
 
-
-
 /*########################################code from caesar#########################################*/
 /*########################################code from caesar#########################################*/
 /*########################################code from caesar#########################################*/
@@ -259,8 +249,6 @@ For security, you’ll see asterisks (*) instead of the actual characters in you
 /*----------------------- sipher the text but this time leter by letter  ---------------------*/
 //1- take every letter from inpute and compare it to what it equal to in asscii location
 //2- convert the letter to what it is location is in the key
-
-
 
 /*----------------------- sipher the text but this time leter by letter  ---------------------*/
 //becuse we everty letter in itis own is key
