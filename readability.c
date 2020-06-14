9e7d7e51bf946d85abca020f847368a2dd2bda38
THEKINGSTAR <THEKINGSTAR5002@GMAIL.COM>
2020-06-14 05:09:11 +0200

    ADD TEST RESULT FOR readability

--- readability.c
+++ readability.c
@@ -69,3 +69,18 @@ Text: When he was nearly thirteen, my brother Jem got his arm badly broken at th
 //295 letter(s)
 //70 word(s)
 //3 sentence(s)
+
+/*------------------------------TEST RESULTS------------------------------*/
+/*
+:) readability.c exists
+:) readability.c compiles
+:) handles single sentence with multiple words
+:) handles punctuation within a single sentence
+:) handles more complex single sentence
+:) handles multiple sentences
+:) handles multiple more complex sentences
+:) handles longer passages
+:) handles questions in passage
+:) handles reading level before Grade 1
+:) handles reading level at Grade 16+
+*/
